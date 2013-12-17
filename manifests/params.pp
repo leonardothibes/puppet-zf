e2081aa37ea45c5d7dc2a908a3889ba7e9ce0bfb
Leonardo Thibes <eu@leonardothibes.com>
2013-12-17 15:43:44 -0200

    implementando classe de parametros

---
+++ manifests/params.pp
@@ -0,0 +1,12 @@
+class zf::params
+{
+	$ensure  = "present"
+	$version = "1.12.3"
+	$srcdir  = "/usr/src"
+	$peardir = "/usr/share/php"
+	$zenddir = "/usr/share/ZendFramework"
+	$pkgname = "ZendFramework-${version}.tar.gz"
+	$zendurl = "http://packages.zendframework.com/releases/ZendFramework-${version}/${pack}"
+	$toolbin = "/usr/bin/zf"
+	$tool    = false
+}
