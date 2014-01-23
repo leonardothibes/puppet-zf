910ac949fbfce46768ff24445a0fe5575435f9b9
Leonardo Thibes <eu@leonardothibes.com>
2014-01-23 14:51:17 -0200

    identacao apenas

--- manifests/dependencies.pp
+++ manifests/dependencies.pp
@@ -4,10 +4,6 @@ class zf::dependencies
 		fail("Unsupported platform: ${osfamily}/${operatingsystem}")
 	}
 	require wget
-    if !defined(Packages['php5']) {
-        package {'php5': ensure  => present}
-    }   
-    if !defined(Packages['php-pear']) {
-        package {'php-pear': ensure  => present}
-    }
+    if !defined(Packages['php5'])     { package {'php5'    : ensure  => present} }
+    if !defined(Packages['php-pear']) { package {'php-pear': ensure  => present} }
 }
