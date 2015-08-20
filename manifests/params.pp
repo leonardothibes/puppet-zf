fa8fd6a0d295d1439950348b88cc12a324f3c34e
Fabian <fabian.schneider@mediafinanz.de>
2015-08-20 12:50:18 +0200

    alignment fixed

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,11 +1,11 @@
 class zf::params
 {
-	$ensure  			= "present"
-	$version 			= "1.12.3"
-	$srcdir  			= "/usr/src"
-	$peardir 			= "/usr/share/php"
-	$zenddir 			= "/usr/share/ZendFramework"
-	$toolbin 			= "/usr/bin/zf"
-	$zftool  			= false
-	$manage_deps  = true
+  $ensure      = "present"
+  $version     = "1.12.3"
+  $srcdir      = "/usr/src"
+  $peardir     = "/usr/share/php"
+  $zenddir     = "/usr/share/ZendFramework"
+  $toolbin     = "/usr/bin/zf"
+  $zftool      = false
+  $manage_deps = true
 }
